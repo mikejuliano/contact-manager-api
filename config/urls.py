6ab3a254ae4fb323f33e4886c43e4cd556b57ab5
Mike Juliano <mjuliano@buildinmotion.com>
2018-04-20 15:42:20 -0400

    token url working

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r"^$", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -21,6 +22,7 @@ urlpatterns = [
     ),
     url(r"^accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    url(r'^api-token-auth/', views.obtain_auth_token),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
